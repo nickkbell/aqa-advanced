b3f4ce0066ddd8c46d475ee4ab93b50f9485b03d
nick <n.belitskiy@infomir.com>
2024-06-15 13:48:19 +0300

    homework 5

---
+++ js/task1.js
@@ -0,0 +1,14 @@
+const averageGrade = 80;
+
+
+if (averageGrade >= 91 && averageGrade <= 100) {
+    console.log('Відмінно');
+} else if (averageGrade >= 81 && averageGrade <= 90) {
+    console.log('Дуже добре');
+} else if (averageGrade >= 71 && averageGrade <= 80) {
+    console.log('Добре');
+} else if (averageGrade >= 61 && averageGrade <= 70) {
+    console.log('Задовільно');
+} else {
+    console.log('Незадовільно');
+}
---
+++ js/task3.js
@@ -0,0 +1,15 @@
+let number = 5;
+let counter = 1;
+let result;
+
+for (let i = 1; i <= 10; i++) {
+    result = number * i;
+    console.log(`${number} x ${i} = ${result}`);
+}
+
+
+while (counter <= 10) {
+    result = number * counter;
+    console.log(`${number} x ${counter} = ${result}`);
+    counter++;
+}
